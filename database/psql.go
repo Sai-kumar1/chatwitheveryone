600b4c726fd28c71cdd91a6543323dccf95a721c
sai <saikumar.nitap@gmail.com>
2022-03-16 16:18:50 +0530

    Older version using DB

--- database/psql.go
+++ database/psql.go
@@ -1,13 +1,15 @@
 package database
 
-import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
-	"net/http"
+// import (
+// 	// "database/sql"
+// 	// "encoding/json"
+// 	// "fmt"
+// 	// "io/ioutil"
+// 	// "net/http"
 
-	_ "github.com/lib/pq"
-)
+// 	_ "github.com/lib/pq"
+// 	// "gopkg.in/yaml.v2"
+// )
 
 type Message struct {
 	To      string `json:"to"`
@@ -20,57 +22,67 @@ type User struct {
 	To string `json:"user"`
 }
 
-var psql *sql.DB
+// type psqlConfig struct {
+// 	PSQLURI string `yaml:"psql_uri"`
+// }
 
-func ConnectToDB(){
-	// for dev
-	// host := config["url"]
-	// port := config["port"]
-	// user := config["user"]
-	// password := config["password"]
-	// dbname := config["db"]
+// var psql *sql.DB
 
-	psqlConn := "postgres://qzdiorjrtvqbyz:521a5ad9cc01b09f06b81b6c9eed3eb7a8d0b00e077453085e587aaa3e8cdf90@ec2-3-216-221-31.compute-1.amazonaws.com:5432/d91n7256lpp7sb"
-	var er error
-	psql, er = sql.Open("postgres", psqlConn)
-	if er!=nil{
-		fmt.Println(er)
-	}
-	
-}
+// func ConnectToDB() {
+// 	yamlData, err := ioutil.ReadFile("./config.yml")
+// 	if err != nil {
+// 		fmt.Println("unable to read config.yml file")
+// 	}
+// 	var config psqlConfig
+// 	yaml.Unmarshal(yamlData, &config)
 
-func GetUserMessages(w http.ResponseWriter,user User) {
-	fmt.Println(user,user.To)
-	sqlQuery := fmt.Sprintf("SELECT * FROM messages WHERE to_user = '%v'", user.To)
-	fmt.Println(sqlQuery)
-	data, er := psql.Query(sqlQuery)
-	delSqlQuery := fmt.Sprintf("DELETE FROM messages WHERE to_user='%v'",user.To)
-	_, errrr := psql.Query(delSqlQuery)
-	if errrr!=nil{
-		fmt.Println(errrr)
-	}
-	if er != nil {
-		fmt.Println("query error",er)
-	}
-	var manyMsgs []Message
-	for data.Next() {
-		var perMsg Message
-		err := data.Scan(&perMsg.To, &perMsg.From, &perMsg.Message, &perMsg.Time)
-		if err != nil {
-			fmt.Println("Error Looping data", err)
-		}
-		manyMsgs = append(manyMsgs, perMsg)
-	}
-	w.Header().Set("Content-Type","application/json")
-	json.NewEncoder(w).Encode(manyMsgs)
+// 	psqlConn := config.PSQLURI
+// 	var er error
+// 	psql, er = sql.Open("postgres", psqlConn)
+// 	if er != nil {
+// 		fmt.Println(er)
+// 	} else {
+// 		fmt.Println("psql connected....")
+// 	}
 
-}
+// }
 
-func InsertMessage(msg Message) {
+// func GetUserMessages(w http.ResponseWriter, user User) []Message {
+// 	fmt.Println(user, user.To)
+// 	sqlQuery := fmt.Sprintf("SELECT * FROM messages WHERE to_user = '%v'", user.To)
+// 	// fmt.Println(sqlQuery)
+// 	data, er := psql.Query(sqlQuery)
 
-	sqlQuery := `INSERT INTO messages(to_user,from_user,message,time) VALUES($1,$2,$3,$4);`
-	_, er := psql.Exec(sqlQuery, msg.To, msg.From, msg.Message, msg.Time)
-	if er != nil {
-		fmt.Println(er)
-	}
-}
+// 	defer data.Close()
+// 	delSqlQuery := fmt.Sprintf("DELETE FROM messages WHERE to_user='%v'", user.To)
+// 	delData, errrr := psql.Query(delSqlQuery)
+// 	defer delData.Close()
+// 	if errrr != nil {
+// 		fmt.Println(errrr)
+// 	}
+// 	if er != nil {
+// 		fmt.Println("query error", er)
+// 	}
+// 	var manyMsgs []Message
+// 	for data.Next() {
+// 		var perMsg Message
+// 		err := data.Scan(&perMsg.To, &perMsg.From, &perMsg.Message, &perMsg.Time)
+// 		if err != nil {
+// 			fmt.Println("Error Looping data", err)
+// 		}
+// 		manyMsgs = append(manyMsgs, perMsg)
+// 	}
+// 	// w.Header().Set("Content-Type","application/json")
+// 	// json.NewEncoder(w).Encode(manyMsgs)
+// 	return manyMsgs
+
+// }
+
+// func InsertMessage(msg Message) {
+
+// 	sqlQuery := `INSERT INTO messages(to_user,from_user,message,time) VALUES($1,$2,$3,$4);`
+// 	_, er := psql.Exec(sqlQuery, msg.To, msg.From, msg.Message, msg.Time)
+// 	if er != nil {
+// 		fmt.Println(er)
+// 	}
+// }
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,38 +3,90 @@ package handlers
 import (
 	db "chat/database"
 	"encoding/json"
+	"fmt"
+
+	// "fmt"
+	// "sync"
+	"time"
+
 	// "fmt"
 	"net/http"
 )
 
+// var requestMap map[string]*http.Request = make(map[string]*http.Request)
+// var responseWriterMap map[string]http.ResponseWriter = make(map[string]http.ResponseWriter)
 
+// var mutex sync.Mutex
 
-func HomeHandler(w http.ResponseWriter,r *http.Request){
-	if r.Method=="GET"{
-		http.ServeFile(w,r,"./static/html/homepage.html")
-	}
-	
+var messages map[string][]db.Message = make(map[string][]db.Message)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+
+	http.ServeFile(w, r, "./static/html/homepage.html")
+
 }
 
-func SendMsgHandler(w http.ResponseWriter,r *http.Request){
-	if r.Method == "POST"{
-		var msg db.Message
-		er := json.NewDecoder(r.Body).Decode(&msg)
-		if er!=nil{
-			http.Error(w, er.Error(), http.StatusBadRequest)
-		}
-		db.InsertMessage(msg)
+func SendMsgHandler(w http.ResponseWriter, r *http.Request) {
+
+	var msg db.Message
+	er := json.NewDecoder(r.Body).Decode(&msg)
+	if er != nil {
+		http.Error(w, er.Error(), http.StatusBadRequest)
 	}
+	// if msg.To is not having an active request then insert in db
+	// else respond to active request
+	// if requestMap[msg.To] != nil {
+		// RecMsgHandler(responseWriterMap[msg.To], requestMap[msg.To])
+		// mutex.Lock()
+		// delete(requestMap, msg.To)
+		// delete(responseWriterMap,msg.To)
+		// mutex.Unlock()
+		// w.WriteHeader(http.StatusOK)
+	// } else {
+		// db.InsertMessage(msg)
+	// }
+	// fmt.Println(requestMap)
+	// db.InsertMessage(msg)
+	messages[msg.To] = append(messages[msg.To],msg)
+		fmt.Println(messages)
+
 }
 
+func RecMsgHandler(w http.ResponseWriter, r *http.Request) {
 
-func RecMsgHandler(w http.ResponseWriter,r *http.Request){
-	if r.Method == "POST"{
-		var user db.User
-		er := json.NewDecoder(r.Body).Decode(&user)
-		if er != nil{
-			http.Error(w, er.Error(), http.StatusBadRequest)
-		}
-		db.GetUserMessages(w,user)
+	var user db.User
+	er := json.NewDecoder(r.Body).Decode(&user)
+	if er != nil {
+		http.Error(w, er.Error(), http.StatusBadRequest)
 	}
+
+	// msgs := db.GetUserMessages(w, user)
+	// fmt.Println(msgs)
+	for i:=0;i<10;i++{
+		// msgs := db.GetUserMessages(w, user)
+		msgs := messages[user.To]
+		if len(msgs) > 0 {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(msgs)
+			delete(messages,user.To)
+			return
+		}
+		time.Sleep(time.Second)
+		
+	}
+	// if len(msgs) > 0 {
+	// 	// fmt.Println(requestMap,responseWriterMap)
+	// 	// fmt.Println(w,"\n",r)
+	// 	w.Header().Set("Content-Type", "application/json")
+	// 	json.NewEncoder(w).Encode(msgs)
+	// } 
+	// else {
+		// mutex.Lock()
+		// requestMap[user.To] = r
+		// responseWriterMap[user.To] = w
+		// mutex.Unlock()
+		// w.Header().Set("Connection","Keep-Alive")
+	// }
+	// fmt.Println(requestMap)
+	// fmt.Println(r,"\n",w)
 }
